417b82ccd42cc549e95ced78a27b5e2482002997
Carden Z <ephemere@protonmail.com>
2018-12-06 04:49:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+	position = board[index]
+	case position
+  when "X"
+		true
+  when "0"
+		true
+	when " "
+		false
+  when ""
+		false
+	when nil
+		false
+	end
+end
